8fd2804aec39d31ee962c3315870e264ba26538b
Stéphane CHEA <19475666+Eriphid@users.noreply.github.com>
2019-06-18 14:49:58 +0200

    add typescript

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,19 @@
-"use strict";
+interface HTMLCollection {
+    [Symbol.iterator]: () => Iterator<HTMLElement>
+}
+interface NodeListOf<TNode> {
+    [Symbol.iterator]: () => Iterator<TNode>
+}
+
+interface Shape extends SVGElement {
+    is_blank?: boolean
+    setAttribute(name: string, value: string | number): void
+}
+
 (function () {
     const scene = document.getElementById("class-or-not");
-    function create_grid(size) {
+
+    function create_grid(size: number) {
         const grid_center = Math.round(size / 2) - 1;
         const grid_size = 1000;
         scene.setAttributeNS(null, "viewBox", `0 0 ${grid_size} ${grid_size}`);
@@ -9,17 +21,20 @@
             size: grid_size / size,
             center: (grid_size / size) / 2,
             padding: grid_size / size * 0.08
-        };
-        let grid = [];
+        }
+
+        let grid: Shape[][] = [];
         for (let y = 0; y < size; ++y) {
-            let line = [];
+            let line: Shape[] = [];
             grid.push(line);
             for (let x = 0; x < size; ++x) {
-                let shape;
+                let shape: Shape;
+
                 let is_blank_desc = {
                     get: () => shape.classList.contains("blank"),
-                    set: (value) => shape.classList[value ? "add" : "remove"]("blank")
-                };
+                    set: (value: boolean) => shape.classList[value ? "add" : "remove"]("blank")
+                }
+
                 if (x !== y) {
                     // Circle
                     shape = scene.appendChild(document.createElementNS("http://www.w3.org/2000/svg", "circle"));
@@ -30,11 +45,13 @@
                 else {
                     // Rectangle
                     shape = scene.appendChild(document.createElementNS("http://www.w3.org/2000/svg", "rect"));
+
                     let coord = {
-                        x: null,
-                        y: null,
+                        x: null as number,
+                        y: null as number,
                         size: item.size - item.padding * 2
-                    };
+                    }
+
                     // Losange
                     if (x === grid_center) {
                         coord.size /= Math.sqrt(2);
@@ -45,16 +62,14 @@
                         is_blank_desc.set = (value) => {
                             shape.classList[value ? "add" : "remove"]("blank");
                             for (let x2 = 0; x2 < size; ++x2) {
-                                if (x2 === x)
-                                    continue;
+                                if (x2 === x) continue;
                                 grid[y][x2].is_blank = !grid[y][x2].is_blank;
                             }
                             for (let y2 = 0; y2 < size; ++y2) {
-                                if (y2 === y)
-                                    continue;
+                                if (y2 === y) continue;
                                 grid[y2][x].is_blank = !grid[y2][x].is_blank;
                             }
-                        };
+                        }
                     }
                     else {
                         coord.x = x * item.size + item.padding;
@@ -62,33 +77,36 @@
                         is_blank_desc.set = (value) => {
                             shape.classList[value ? "add" : "remove"]("blank");
                             for (let x2 = 0; x2 < size; ++x2) {
-                                if (x2 === x)
-                                    continue;
+                                if (x2 === x) continue;
                                 grid[y][x2].is_blank = !grid[y][x2].is_blank;
                             }
                             for (let y2 = 0; y2 < size; ++y2) {
-                                if (y2 === y)
-                                    continue;
+                                if (y2 === y) continue;
                                 grid[y2][x].is_blank = value;
                             }
-                        };
+                        }
                     }
                     shape.setAttribute("x", coord.x);
                     shape.setAttribute("y", coord.y);
                     shape.setAttribute("width", coord.size);
                     shape.setAttribute("height", coord.size);
                 }
+
                 shape.addEventListener("click", (ev) => {
                     shape.is_blank = !shape.is_blank;
-                });
+                })
+
                 Object.defineProperty(shape, "is_blank", is_blank_desc);
                 line.push(shape);
             }
         }
     }
+
     create_grid(15);
+
     const blank_btn = document.getElementById("all-blank");
     const black_btn = document.getElementById("all-black");
+
     blank_btn.addEventListener("click", () => {
         for (let child of scene.children) {
             child.classList.add("blank");
@@ -99,4 +117,4 @@
             child.classList.remove("blank");
         }
     });
-})();
+})()
